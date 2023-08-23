9865f5b0583b025e635a08dfeee8999437e23e45
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-08-23 15:05:21 +0300

    23.08.2023

--- generics.ts
+++ generics.ts
@@ -14,6 +14,7 @@ console.log(sehir)
 // deger x e number type i verince string, string type i verince number yazamiyoruz
 // Ama T ile type i belirtir sonra x degeri yazarken type i belirtirsek sorun olmuyor 
 // generic deniyor buna. 
+
 function deger3<T>(x:T):T{
     return x;
 }
@@ -22,7 +23,6 @@ let sayi3 = deger3<number>(2)
 let sehir3= deger3<string>('Istanbul')
 console.log(sayi3)
 console.log(sehir3)
-
 
 // generics leri class olarak da kullanabiliyoruz
 
